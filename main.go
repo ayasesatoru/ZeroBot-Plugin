b4ba15bc40732a3b534a738f2aa2e73936663006
ayasesatoru <113226501+ayasesatoru@users.noreply.github.com>
2022-09-11 11:05:24 +0000

    recommand

--- main.go
+++ main.go
@@ -239,7 +239,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", "珊瑚宫"),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
